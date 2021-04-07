3c708dcf811a9e0482da370d5c41d8e2fe80f029
polnaharro <82111376+polnaharro@users.noreply.github.com>
2021-04-07 19:08:45 +0200

    Add files via upload

---
+++ P3-A-Pol-Naharro/src/main.cpp
@@ -0,0 +1,53 @@
+#include <Arduino.h>
+
+/*
+ESP32 Web Server - STA Mode
+modified on 25 MAy 2019
+by Mohammadreza Akbari @ Electropeak
+https://electropeak.com/learn
+*/
+
+#include <WiFi.h>
+#include <WebServer.h>
+// SSID & Password
+const char* ssid = "MOVISTAR_5863"; // Enter your SSID here
+const char* password = "28vcTuh9WXePa2Fq58zj"; //Enter your Password here
+WebServer server(80); // Object of WebServer(HTTP port, 80 is defult)
+
+void handle_root(void);
+
+
+void setup() {
+Serial.begin(115200);
+Serial.println("Try Connecting to ");
+Serial.println(ssid);
+// Connect to your wi-fi modem
+WiFi.begin(ssid, password);
+// Check wi-fi is connected to wi-fi network
+while (WiFi.status() != WL_CONNECTED) {
+delay(1000);
+Serial.print(".");
+}
+Serial.println("");
+Serial.println("WiFi connected successfully");
+Serial.print("Got IP: ");
+Serial.println(WiFi.localIP()); //Show ESP32 IP on serial
+server.on("/", handle_root);
+server.begin();
+Serial.println("HTTP server started");
+delay(100);
+}
+void loop() {
+server.handleClient();
+}
+// HTML & CSS contents which display on web server
+String HTML = "<!DOCTYPE html>\
+<html>\
+<body>\
+<h1>My Primera Pagina con ESP32 - Station Mode &#128522;</h1>\
+</body>\
+</html>";
+// Handle root url (/)
+void handle_root() {
+server.send(200, "text/html", HTML);
+}
